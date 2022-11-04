11118d221558f41a2029b889c3a771da10d18730
sommergeo <26389306+sommergeo@users.noreply.github.com>
2022-11-04 19:48:49 +0100

    venn diagram and cartogram

--- fig_venn.R
+++ fig_venn.R
@@ -13,7 +13,8 @@ x <- list(
                col_names = FALSE, locale = locale(encoding = "ASCII"))$X1
 )
 
-ggVennDiagram(x, set_color = 'black', label='both', set_size=3, label_size = 2,
+# Venn diagram ----
+ggVennDiagram(x, set_color = 'black', label='both', set_size=3, label_size = 2, edge_size = 0.5,
               category.names = c('Cultural remains', 'Human remains', 'Plant remains', 'Faunal remains'))+
   scale_x_continuous(expand = expansion(mult = .2))+
   scale_y_continuous(expand = expansion(mult = .2))+
@@ -21,16 +22,34 @@ ggVennDiagram(x, set_color = 'black', label='both', set_size=3, label_size = 2,
                       low="#deebf7", high = "#3182bd",
                       trans='log', breaks=c(1, 10,100,1000), labels=c(1,10,100,1000),
                       limits=c(1,1000))+
+  #scale_fill_gradient(name='Number of\nLocalities',
+  #                    low="#deebf7", high = "#3182bd",
+  #                    breaks=c(0,200,400,600,800), labels=c(0,200,400,600,800),
+  #                    limits=c(0,800))+
   scale_color_manual(values=c('black','black','black','black'))+
   theme(text=element_text(size=8), #change font size of all text
         plot.title=element_text(size=8), #change font size of plot title
         legend.text=element_text(size=8), #change font size of legend text
         legend.title=element_text(size=8))
 
-ggsave('fig_venn/fig_venn_v3.tiff', width=120, height=80, units='mm', dpi=600)
-ggsave('fig_venn/fig_venn_v3.png', width=120, height=80, units='mm', dpi=300, bg='white')
+ggsave('fig_venn/fig_venn_v2.tiff', width=120, height=80, units='mm', dpi=300)
+ggsave('fig_venn/fig_venn_v2.png', width=120, height=80, units='mm', dpi=300, bg='white')
   
 
+
+
+
+# Stats ----
+data <- process_data(venn)
+stat_categories <- lengths(x)  # number of sites per category
+stat_total <- c(x$A, x$B, x$C, x$D) %>% unique() %>% length() # number of sites in total category
+stat_single <- data@region %>% filter(id %in% c(1,2,3,4)) %>% pull(count) %>% sum()   # number of loclaities with 1 category
+stat_double <- data@region %>% filter(id %in% c(12,13,14,23,24,34)) %>% pull(count) %>% sum()   # number of loclaities with 2 categories
+stat_triple <- data@region %>% filter(id %in% c(123,124,134,234)) %>% pull(count) %>% sum()   # number of loclaities with 3 categories
+stat_quadruple <- data@region %>% filter(id %in% c(1234)) %>% pull(count) %>% sum()   # number of loclaities with 4 categories
+
+
+# Old stuff ----
 venn <- Venn(x)
 data <- process_data(venn)
 ggplot() +
@@ -38,7 +57,7 @@ ggplot() +
   geom_sf(aes(fill = count), data = venn_region(data)) +
   # 2. set edge layer
   #geom_sf(aes(color = id), data = venn_setedge(data), show.legend = FALSE) +
-  geom_sf(color="black", size = .5, data = venn_setedge(data), show.legend = FALSE) +  
+  geom_sf(color="black", size = .2, data = venn_setedge(data), show.legend = FALSE) +  
   # 3. set label layer
   #geom_sf_text(aes(label = name), data = venn_setlabel(data)) +
   geom_sf_text(label = c('Cultural remains', 'Human remains', 'Plant remains', 'Faunal remains'), data = venn_setlabel(data), size=3) +
@@ -61,4 +80,3 @@ ggsave('fig_venn/fig_venn_v2.png', width=120, height=80, units='mm', dpi=300, bg
 
 # Resources
 # https://gaospecial.github.io/ggVennDiagram/articles/fully-customed
-
